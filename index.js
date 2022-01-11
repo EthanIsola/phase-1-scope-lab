f1180adcfa584222ef7f138a678bc5a2b25ff1df
Ethan Isola <isolaethan@gmail.com>
2022-01-11 09:01:36 -0600

    commit tree a2877d88e906db4a32e6d643ee17483b1121f5e1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(best){
+    bestCustomer = best;
+}
+
+const leastFavoriteCustomer = "Karen";
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+}
